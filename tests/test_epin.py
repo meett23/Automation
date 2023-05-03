b6f2614aa9424ba5cf1eb02e7e5394fa100b9337
meett23 <87054800+meett23@users.noreply.github.com>
2023-05-03 18:37:21 +0530

    add testcases for EPIN

--- tests/test_epin.py
+++ tests/test_epin.py
@@ -297,6 +297,12 @@ class TestCaseForEPIN(unittest.TestCase):
             # Run this part if button is disabled
             self.driver.close()
 
+
+
+#  Epin with buying for self
+
+
+
     def test_buy_epin_buying_for_self_data(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get("https://sd-epin.bustto.com/")
@@ -352,6 +358,269 @@ class TestCaseForEPIN(unittest.TestCase):
             # Run this part if button is disabled
             self.driver.close()
         
+
+    
+    def test_success_buy_epin_buying_for_self_data(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.get("https://sd-epin.bustto.com/")
+        self.driver.maximize_window()
+        time.sleep(1)
+
+        self.driver.find_element(
+            By.XPATH,
+            "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[1]/div/div/div[1]/button[2]",
+        ).click()
+        self.driver.find_element(
+            By.XPATH,
+            "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[2]/div/div/button[2]",
+        ).click()
+
+        # Select to Buying for self
+        self.driver.find_element(By.XPATH, "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[3]/div/button[2]").click()
+
+        # buying for self in all FIELD IS BLANK OR # Fill information in buying for self
+        self.driver.find_element(By.XPATH, "//*[@id=':rb:']").send_keys("Meet Thakkar")     # enter name
+        self.driver.find_element(By.XPATH, "//*[@id=':rc:']").send_keys("meet164023@gmail.com")     # enter email
+        self.driver.find_element(By.XPATH, "//*[@id=':rp:']").send_keys("9106612343")      # enter phone number
+
+        # This is not required in web. It's optional.
+        # self.driver.find_element(
+        #     By.CSS_SELECTOR,
+        #     "#scrollable-auto-tabpanel-1 > h6 > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-md-7.css-1odkrke > div:nth-child(4) > div > div:nth-child(4) > div > div > svg",
+        # ).click()      
+
+        element = self.driver.find_element(
+            By.XPATH,
+            "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[6]/div/div[2]",
+        )
+    
+        # scroll till 'buy now' button is not find /
+        self.driver.execute_script(
+            "window.scrollBy(0,1000)", " "
+        )  # scroll till 1000 pixel /
+        time.sleep(2)
+
+        self.driver.find_element(
+            By.XPATH,
+            "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[6]/div/div[2]",
+        ).click()
+        time.sleep(1)
+
+        # button = self.driver.find_element(By.XPATH, '//*[@id="scrollable-auto-tabpanel-1"]/h6/div/div[2]/div[6]/div/div[2]')
+        # button.click()
+        # if button.is_enabled():
+        #     # Run the part when button is enabled
+        #     button.click()
+        # else:
+        #     # Run this part if button is disabled
+        #     self.driver.close()
+        
+        self.driver.find_element(By.XPATH, "/html/body/div[2]/div[3]/div/div/div[2]/button[2]").click()
+        time.sleep(3)
+        self.driver.close()
+
+    
+
+    def test_buy_epin_buying_for_self_email(self):           # Entered wrong email
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.get("https://sd-epin.bustto.com/")
+        self.driver.maximize_window()
+        time.sleep(1)
+
+        self.driver.find_element(
+            By.XPATH,
+            "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[1]/div/div/div[1]/button[2]",
+        ).click()
+        self.driver.find_element(
+            By.XPATH,
+            "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[2]/div/div/button[2]",
+        ).click()
+
+        # Select to Buying for self
+        self.driver.find_element(By.XPATH, "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[3]/div/button[2]").click()
+
+        # buying for self in all FIELD IS BLANK OR # Fill information in buying for self
+        self.driver.find_element(By.XPATH, "//*[@id=':rb:']").send_keys("Meet Thakkar")     # enter name
+        self.driver.find_element(By.XPATH, "//*[@id=':rc:']").send_keys("meet164023@")     # enter email
+        self.driver.find_element(By.XPATH, "//*[@id=':rp:']").send_keys("9106612343")      # enter phone number
+
+                                            # This is not required in web. It's optional.
+                                            # self.driver.find_element(
+                                            #     By.CSS_SELECTOR,
+                                            #     "#scrollable-auto-tabpanel-1 > h6 > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-md-7.css-1odkrke > div:nth-child(4) > div > div:nth-child(4) > div > div > svg",
+                                            # ).click()      
+
+        element = self.driver.find_element(
+            By.XPATH,
+            "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[6]/div/div[2]",
+        )
+    
+        # scroll till 'buy now' button is not find /
+        self.driver.execute_script(
+            "window.scrollBy(0,1000)", " "
+        )  # scroll till 1000 pixel /
+        time.sleep(2)
+
+        # self.driver.find_element(
+        #     By.XPATH,
+        #     "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[6]/div/div[2]",
+        # ).click()
+        # time.sleep(1)
+
+        button = self.driver.find_element(By.XPATH, '//*[@id="scrollable-auto-tabpanel-1"]/h6/div/div[2]/div[6]/div/div[2]')
+        # button.click()
+        if button.is_enabled():
+            # Run the part when button is enabled
+            button.click()
+            print(" "*200)
+            print(" "*200)
+            print("*"*200)
+            print(" "*200)
+            print(" "*200)
+        else:
+            # Run this part if button is disabled
+            self.driver.close()
+    
+
+    def test_buy_epin_buying_for_self_mobile(self):           # Entered wrong mobile
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.get("https://sd-epin.bustto.com/")
+        self.driver.maximize_window()
+        time.sleep(1)
+
+        self.driver.find_element(
+            By.XPATH,
+            "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[1]/div/div/div[1]/button[2]",
+        ).click()
+        self.driver.find_element(
+            By.XPATH,
+            "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[2]/div/div/button[2]",
+        ).click()
+
+        # Select to Buying for self
+        self.driver.find_element(By.XPATH, "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[3]/div/button[2]").click()
+
+        # buying for self in all FIELD IS BLANK OR # Fill information in buying for self
+        self.driver.find_element(By.XPATH, "//*[@id=':rb:']").send_keys("Meet Thakkar")     # enter name
+        self.driver.find_element(By.XPATH, "//*[@id=':rc:']").send_keys("meet164023@gmail.com")     # enter email
+        self.driver.find_element(By.XPATH, "//*[@id=':rp:']").send_keys("91066")      # enter phone number
+
+                                            # This is not required in web. It's optional.
+                                            # self.driver.find_element(
+                                            #     By.CSS_SELECTOR,
+                                            #     "#scrollable-auto-tabpanel-1 > h6 > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-md-7.css-1odkrke > div:nth-child(4) > div > div:nth-child(4) > div > div > svg",
+                                            # ).click()      
+
+        element = self.driver.find_element(
+            By.XPATH,
+            "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[6]/div/div[2]",
+        )
+    
+        # scroll till 'buy now' button is not find /
+        self.driver.execute_script(
+            "window.scrollBy(0,1000)", " "
+        )  # scroll till 1000 pixel /
+        time.sleep(2)
+
+        # self.driver.find_element(
+        #     By.XPATH,
+        #     "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[6]/div/div[2]",
+        # ).click()
+        # time.sleep(1)
+
+        button = self.driver.find_element(By.XPATH, '//*[@id="scrollable-auto-tabpanel-1"]/h6/div/div[2]/div[6]/div/div[2]')
+        # button.click()
+        if button.is_enabled():
+            # Run the part when button is enabled
+            button.click()
+            print(" "*200)
+            print(" "*200)
+            print("*"*200)
+            print(" "*200)
+            print(" "*200)
+        else:
+            # Run this part if button is disabled
+            self.driver.close()
+
+
+# denominaton test greater than 10000
+
+
+    def test_denomination_limit_in_buying_for_self(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.get("https://sd-epin.bustto.com/")
+        self.driver.maximize_window()
+        time.sleep(1)
+
+        self.driver.find_element(
+            By.XPATH,
+            "//*[@id=':ra:']",
+        ).send_keys("00")
+        # time.sleep(3)
+        
+        Quantity_button = self.driver.find_element(
+            By.XPATH,
+            "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[2]/div/div/button[2]",
+        )
+
+        if Quantity_button.is_enabled():
+            print("*"*100)
+            print("*"*100)
+            Quantity_button.click()
+            print("*"*100)
+            print("*"*100)
+        
+        else:
+            self.driver.close()
+
+
+
+        # Select to Buying for self
+        # self.driver.find_element(By.XPATH, "----------------//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[3]/div/button[2]").click()
+
+        # # buying for self in all FIELD IS BLANK OR # Fill information in buying for self
+        # self.driver.find_element(By.XPATH, "//*[@id=':rb:']").send_keys("Meet Thakkar")     # enter name
+        # self.driver.find_element(By.XPATH, "//*[@id=':rc:']").send_keys("meet164023@gmail.com")     # enter email
+        # self.driver.find_element(By.XPATH, "//*[@id=':rp:']").send_keys("91066")      # enter phone number
+
+        #                                     # This is not required in web. It's optional.
+        #                                     # self.driver.find_element(
+        #                                     #     By.CSS_SELECTOR,
+        #                                     #     "#scrollable-auto-tabpanel-1 > h6 > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-md-7.css-1odkrke > div:nth-child(4) > div > div:nth-child(4) > div > div > svg",
+        #                                     # ).click()      
+
+        # element = self.driver.find_element(
+        #     By.XPATH,
+        #     "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[6]/div/div[2]",
+        # )
+    
+        # # scroll till 'buy now' button is not find /
+        # self.driver.execute_script(
+        #     "window.scrollBy(0,1000)", " "
+        # )  # scroll till 1000 pixel /
+        # time.sleep(2)
+
+        # # self.driver.find_element(
+        # #     By.XPATH,
+        # #     "//*[@id='scrollable-auto-tabpanel-1']/h6/div/div[2]/div[6]/div/div[2]",
+        # # ).click()
+        # # time.sleep(1)
+
+        # button = self.driver.find_element(By.XPATH, '//*[@id="scrollable-auto-tabpanel-1"]/h6/div/div[2]/div[6]/div/div[2]')
+        # # button.click()
+        # if button.is_enabled():
+        #     # Run the part when button is enabled
+        #     button.click()
+        #     print(" "*200)
+        #     print(" "*200)
+        #     print("*"*200)
+        #     print(" "*200)
+        #     print(" "*200)
+        # else:
+        #     # Run this part if button is disabled
+        #     self.driver.close()
+        
+
 
 
 
